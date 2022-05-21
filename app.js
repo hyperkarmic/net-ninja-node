2e1399a21be345fd26d41d6dd871149a748e24d6
hyperkarmic <randy_pan@yahoo.com>
2022-05-21 20:45:13 +0100

    a nice discussion of MVC - unlike fraudulent joke bootcamp.  this needs to be studied and gone over

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+
+const blogRoutes = require('./routes/blogRoutes')
+console.log(blogRoutes)
 
 // express app
 const app = express()
@@ -38,59 +40,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' })
 })
-
-// blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' })
-})
-
-app.get('/blogs', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { blogs: result, title: 'All blogs' })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-app.post('/blogs', (req, res) => {
-  console.log(req.body)
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect('/blogs')
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
+// blog routes (gone to routers folder)
+app.use('/blogs', blogRoutes)
+//look we're using the router, just like middleware!!!!
 
 // 404 page
 app.use((req, res) => {
